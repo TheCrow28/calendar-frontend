c909a9266ec4535669c9cfd9857bf71717330a10
Giorgio Di Filippo <giorgio.difilippo@eng.it>
2023-06-09 20:54:17 +0200

    calendar

---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,80 @@
+import {Component} from '@angular/core';
+import {ChangeDetectionStrategy} from '@angular/core';
+import {startOfDay, endOfDay, subDays, addDays, endOfMonth, isSameDay, isSameMonth, addHours,} from 'date-fns';
+import {Subject} from 'rxjs';
+import {CalendarEvent, CalendarView,} from 'angular-calendar';
+
+@Component({
+  selector: 'app-calendar',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: [],
+  templateUrl: './calendar.component.html',
+
+})
+
+export class CalendarComponent {
+
+  view: CalendarView = CalendarView.Month;
+
+  CalendarView = CalendarView;
+
+  viewDate: Date = new Date();
+
+  refresh = new Subject<void>();
+
+  events: CalendarEvent[] = [
+    {
+      start: subDays(startOfDay(new Date()), 1),
+      end: addDays(new Date(), 1),
+      title: 'A 3 day event',
+    },
+    {
+      start: startOfDay(new Date()),
+      title: 'An event with no end date',
+    },
+    {
+      start: subDays(endOfMonth(new Date()), 3),
+      end: addDays(endOfMonth(new Date()), 3),
+      title: 'A long event that spans 2 months',
+    },
+    {
+      start: addHours(startOfDay(new Date()), 2),
+      end: addHours(new Date(), 2),
+      title: 'An event',
+    },
+  ];
+
+  activeDayIsOpen: boolean = true;
+
+  dayClicked({date, events}: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      this.activeDayIsOpen = !((isSameDay(this.viewDate, date) && this.activeDayIsOpen) ||
+        events.length === 0);
+      this.viewDate = date;
+    }
+  }
+
+  addEvent(): void {
+    this.events = [
+      ...this.events,
+      {
+        title: 'New event',
+        start: startOfDay(new Date()),
+        end: endOfDay(new Date()),
+      },
+    ];
+  }
+
+  deleteEvent(eventToDelete: CalendarEvent) {
+    this.events = this.events.filter((event) => event !== eventToDelete);
+  }
+
+  setView(view: CalendarView) {
+    this.view = view;
+  }
+
+  closeOpenMonthViewDay() {
+    this.activeDayIsOpen = false;
+  }
+}
+
